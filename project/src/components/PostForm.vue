98e9dbcb9a23fa5365a2ce59559c5bb97c0e20f0
Olivia N.A. <oliviaanigbogu860@gmail.com>
2023-10-04 14:38:10 +0100

    main

--- project/src/components/PostForm.vue
+++ project/src/components/PostForm.vue
@@ -82,7 +82,7 @@ export default {
         onFormSubmit(event) {
             event.preventDefault()
             db.collection('users').add(this.user).then(() => {
-                alert("User successfully created!");
+                alert("Post Successfully Created!");
                 this.user.firstName = ''
                 this.user.lastName = ''
                 this.user.phoneNumber = ''
